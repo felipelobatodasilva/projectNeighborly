83bfff3d91ecacfae0788dfdb0b17241330e0628
Felipe <gitlobato@gmail.com>
2021-02-08 03:39:00 -0300

    Changing API_URL

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -16,7 +16,7 @@ API_VERSION = 'v1'
 #API_URL = " https://neighborlyapi.azurewebsites.net/api/"
 
 # for local host if Azure functions served locally
-API_URL = "http://localhost:7071/api"
-#API_URL = "https://neighborlyprojectlegolas.azurewebsites.net/api/"
+#API_URL = "http://localhost:7071/api"
+API_URL = "https://neighborlyprojectlegolas.azurewebsites.net/api/"
 
 # API_URL = "https://legolasproject.azurewebsites.net"
